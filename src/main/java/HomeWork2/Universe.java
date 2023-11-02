f7455bbf2e3d610f636ebd34099e5a41c1c91528
LizaZorina <liza.zorina.98@mail.ru>
2023-11-02 15:41:32 +0400

    2 дз

--- src/main/java/HomeWork2/Universe.java
+++ src/main/java/HomeWork2/Universe.java
@@ -4,6 +4,7 @@ public class Universe {
     public static final long UNIVERSE = 13000000000l; // возраст Вселенной
     public static final int HUMAN = 100000; // возраст человечества
     public static int universeHuman = (int) (UNIVERSE / HUMAN); // во сколько раз Вселенная старше людей
+
     public static void main(String[] args) {
         System.out.println("Возраст Вселенной " + UNIVERSE + " лет");
         System.out.println("Возраст человечества " + HUMAN + " лет");
