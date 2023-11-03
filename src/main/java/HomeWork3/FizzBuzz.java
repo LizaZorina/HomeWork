92531d9a470a801c2e9a02da4101d6a84dd3d49b
LizaZorina <liza.zorina.98@mail.ru>
2023-11-03 13:05:56 +0400

    2 дз

--- src/main/java/HomeWork3/FizzBuzz.java
+++ src/main/java/HomeWork3/FizzBuzz.java
@@ -3,14 +3,14 @@ package HomeWork3;
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 0; i <= 100; i++) {
-            if (i % 3 == 0) {
-                System.out.println(i + " fizz");
-            }
-            if (i % 5 == 0) {
-                System.out.println(i + " buzz");
-            }
             if (i % 15 == 0) { // одновременно 3 и 5 будут делиться на числа, кратные 15
                 System.out.println(i + " FizzBuzz");
+            } else
+            if (i % 5 == 0) {
+                System.out.println(i + " buzz");
+            } else
+            if (i % 3 == 0) {
+                System.out.println(i + " fizz");
             } else System.out.println(i + " ");
         }
     }
