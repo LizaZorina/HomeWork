d124bff2cfa2da27d11390742ef8ddee0a4a6513
LizaZorina <liza.zorina.98@mail.ru>
2023-11-03 14:52:58 +0400

    2 дз

--- src/main/java/HomeWork3/MultiplicationTable.java
+++ src/main/java/HomeWork3/MultiplicationTable.java
@@ -2,50 +2,14 @@ package HomeWork3;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
+        
         for (int i = 1; i <= 9; i++) {
-            int a = i * i;
-
-            System.out.println("1 * " + i + " = " + a);
-        }
-        for (int i = 1; i <= 9; i++) {
-            int b = 2 * i;
-
-            System.out.println("2 * " + i + " = " + b);
-        }
-        for (int i = 1; i <= 9; i++) {
-            int c = 3 * i;
-
-            System.out.println("3 * " + i + " = " + c);
-        }
-        for (int i = 1; i <= 9; i++) {
-            int d = 4 * i;
-
-            System.out.println("4 * " + i + " = " + d);
-        }
-        for (int i = 1; i <= 9; i++) {
-            int e = 5 * i;
-
-            System.out.println("5 * " + i + " = " + e);
-        }
-        for (int i = 1; i <= 9; i++) {
-            int f = 6 * i;
-
-            System.out.println("6 * " + i + " = " + f);
-        }
-        for (int i = 1; i <= 9; i++) {
-            int g = 7 * i;
-
-            System.out.println("7 * " + i + " = " + g);
-        }
-        for (int i = 1; i <= 9; i++) {
-            int h = 8 * i;
-
-            System.out.println("8 * " + i + " = " + h);
-        }
-        for (int i = 1; i <= 9; i++) {
-            int j = 9 * i;
-
-            System.out.println("9 * " + i + " = " + j);
+            System.out.println("Умножение на " + i);
+            int a = 1;
+            while (a <= 9) {
+                System.out.println(i + " * " + a + " = " + i * a);
+                a++;
+            }
         }
     }
 }
